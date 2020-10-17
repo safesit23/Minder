1013f6778cc442cd5cfe675a9c5c90cda28d4aca
Smurf <smurf@Smurfs-MacBook-Pro-2.local>
2020-10-17 16:10:42 +0700

    [INIT]

--- api/index.js
+++ api/index.js
@@ -5,14 +5,20 @@ var io = require('socket.io')(http)
 const service = require('../service/service')
 const port = 3000
 
-app.get('/', (req, res) => {
+let roomno = 1
+let roomCode
+
+app.get('/${room}', (req, res) => {
+    roomCode = req.room
+    console.log(`${roomCode}, ${req.room}`)
     console.log('access')
     res.status(200).sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', (socket) => {
     console.log(`User: ${socket.id} connected`)
-
+    console.log(io.nsps['/'].adapter.rooms["room-"+roomno])
+    socket.join("room-"+roomno)
     socket.on('dropCard', (msg) => {
         console.log(msg)
         service.random()
