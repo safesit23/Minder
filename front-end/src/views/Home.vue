3a2b2d8f9c7c543418782e4e6006a59a4e2ebc66
bankjirapan <jirapan_yankhan@hotmail.com>
2020-10-17 16:28:02 +0700

    [EDIT] Add code

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,53 +1,92 @@
 <template>
- <div class="container">
-      <div class="vertical-center-row">
-        <div class="section-home">
-          <div>
-            <img src="img/mind.png" alt="" srcset="" width="150px">
-          </div>
-          <div class="title-game">
-            <h1>The Minder Game</h1>
-          </div>
-          <div class="row">
-            <div class="col-sm">
-            </div>
-            <div class="col-sm">
-          <div class="player-name ">
-            <input type="text" name="playerName" class="form-control" placeholder="กรุณาใส่ชื่อ">
-          </div>
-            </div>
-            <div class="col-sm">
-            </div>
-          </div>
-          <div class="mt-3">
-            <button class="btn btn-warning btn-lg" @onclick="join()">Join us</button>
-              <!-- <a href="./waitng.html" class="btn btn-warning btn-lg">
-                  Join us
-              </a> -->
+  <div class="d-flex justify-content-center center-screen">
+    <div class="section-home my-auto">
+      <div>
+        <img src="img/mind.png" alt="" srcset="" width="150px" />
+      </div>
+      <div class="title-game">
+        <h1>The Minder Game</h1>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+          <div class="player-name">
+            <input
+              type="text"
+              name="playerName"
+              class="form-control form-name"
+              placeholder="กรุณาใส่ชื่อ"
+            />
+            <br>
+              <input
+              type="text"
+              name="code"
+              class="form-control"
+              :class="showCode === false ? 'hide-code' : ''"
+              placeholder="โค้ดห้อง"
+            />
           </div>
         </div>
+        <div class="col-md-2"></div>
+      </div>
+      <div class="mt-3">
+        <button class="btn btn-success btn-lg" @onclick="join()">
+          Create
+        </button> 
+        &nbsp;
+        <button class="btn btn-warning btn-lg" @click="join()">
+          Join us
+        </button>
+        
+        <!-- <a href="./waitng.html" class="btn btn-warning btn-lg">
+                  Join us
+              </a> -->
       </div>
     </div>
+
+  </div>
 </template>
 
 <script>
-import io from 'socket.io-client'
+import io from "socket.io-client";
 
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
-      socket: io('http://localhost:3000')
-    }
+      showCode : false,
+      socket: io("http://localhost:3000"),
+    };
   },
   mounted() {
-    this.join()
+    this.join();
   },
   methods: {
     join() {
-      this.socket.emit('dropCard', 'hello')
-    }
-  }
-}
+      console.log("aa");
+      this.showCode = true;
+      // this.socket.emit("dropCard", "hello");
+    },
+  },
+};
 </script>
+
+<style>
+.hide-code{
+  display: none;
+}
+.center-screen {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 100vh;
+}
+.form-name{
+  color: #FFF;
+  background:none;
+  border-color: hsl(0, 0%, 100%);
+}
+</style>
 
