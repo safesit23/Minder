eace93b2fae729e09bef7d448127d7cbb4f29e8b
jatawatsafe <jatawatsafe@hotmail.com>
2020-10-17 15:49:19 +0700

    [EDIT] Downgrade to vue-cli 2 and update dependencies

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 import Home from '../views/Home.vue'
 
 const routes = [
@@ -6,22 +7,14 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+Vue.use(VueRouter);
+const router = new VueRouter({
+  mode: "history",
+  linkExactActiveClass: "active",
+  routes,
+});
 
 export default router
