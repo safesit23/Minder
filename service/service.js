f4466af324b9c8bca5e797186b0bf166eb1e4bbe
TipsukonCheer <tipsukon.cheer@mail.kmutt.ac.th>
2020-10-18 17:44:59 +0700

    [UPDATE] restructure to class

--- service/service.js
+++ service/service.js
@@ -1,61 +1,107 @@
-const service = {
-    remainingCards: [],
-    lowestValue: 0,
-    selectedCards: [],
-    init: () => {
-        console.log("init")
-        var ans = [];
-        for (let i = 1; i <= 100; i++) {
-            ans.push(i);
-        }
-        service.remainingCards = ans
-        console.log(service.remainingCards)
-    },
-    getMinValue: () => {
+let Service = class {
+    constructor(code) {
+        this.code = code
+    }
+    players = []
+    remainingCards = []
+    lowestValue = 0
+    selectedCards = []
 
-    },
-    shuffle: () => {
-        let remainingCards = service.remainingCards
-        console.log("shuffle")
+    get player() {
+        return this.player
+    }
+
+    get remainingCards() {
+        return this.remainingCards
+    }
+
+    get lowestValue() {
+        return this.lowestValue
+    }
+
+    get selectedCard() {
+        return this.selectedCards
+    }
+
+    //init 100 cards
+    init() {
+        console.log("---init---")
+        var allCards = [];
+        for (let i = 1; i <= 100; i++) {
+            allCards.push(i);
+        }
+        this.remainingCards = allCards
+        console.log(this.remainingCards)
+    }
+
+    //shuffle card in remaining
+    shuffle() {
+        let remainingCards = this.remainingCards
+        console.log("---shuffle---")
         console.log("before")
         console.log(remainingCards)
         let currentIndex = remainingCards.length, temporaryValue, randomIndex;
 
+        console.log("loop")
         // While there remain elements to shuffle...
         while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
             // And swap it with the current element.
             temporaryValue = remainingCards[currentIndex];
             remainingCards[currentIndex] = remainingCards[randomIndex];
             remainingCards[randomIndex] = temporaryValue;
+            console.log(currentIndex)
         }
-        service.remainingCards = remainingCards
+
+        this.remainingCards = remainingCards
         console.log("after")
-        console.log(service.remainingCards)
-    },
-    checkIfLowest(dropValue) {
-        console.log("---drop---")
-        console.log("before")
-        console.log(service.selectedCards)
-        let remainingSelected = service.selectedCards.filter(e => e != dropValue)
-        service.selectedCards = remainingSelected
-        console.log("after")
-        console.log(service.selectedCards)
-        if (dropValue != service.lowestValue) {
-            console.log("gameOver!")
-            return false
-        }
-        return true;
-    },
-    setPlayerCard(players) {
-        return players.map((player) => {
+        console.log(this.remainingCards)
+    }
+
+    //give card to each player
+    setPlayerCard() {
+        console.log("---setPlayerCard---")
+        return this.players.map((player) => {
             const selectedCard = this.remainingCards.shift();
             this.selectedCards.push(selectedCard)
-            return{
+            this.setLowestValue()
+            return {
                 ...player,
                 cards: [selectedCard],
             }
         })
-    },
+    }
+
+    //get min value of remaining deck
+    setLowestValue() {
+        console.log("---setLowestValue---")
+        this.lowestValue = Math.min(...this.selectedCard);
+        console.log(this.lowestValue)
+    }
+
+    //check if drop card is lowest
+    checkIfGameOver(dropValue) {
+        console.log("---drop---")
+        console.log("before")
+        console.log(this.selectedCards)
+        let remainingSelected = this.selectedCards.filter(e => e != dropValue)
+        this.selectedCards = remainingSelected
+        console.log("after")
+        console.log(this.selectedCards)
+        if (dropValue != this.lowestValue) {
+            console.log("gameOver!")
+            return false
+        }
+        return true;
+    }
+
+    addPlayer(player){
+        console.log("---addPlayer---")
+        console.log(this.players)
+        this.players.push(player)
+    }
 }
 
-module.exports = { ...service }
+module.exports = Service
