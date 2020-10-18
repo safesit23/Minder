89dfe9553ecee539b5b6ab71a533c5cef5279257
TipsukonCheer <tipsukon.cheer@mail.kmutt.ac.th>
2020-10-18 17:45:32 +0700

    [UPDATE] test from service class

--- service/testService.js
+++ service/testService.js
@@ -1,11 +1,22 @@
 const { remainingCards } = require('../service/service');
-const service = require('../service/service')
+const Service = require('../service/service')
 
 const test = {
     testService: () => {
+        let service = new Service("abcde");
+        let players = [
+            { id: 'u1', cards: [] },
+            { id: 'u2', cards: [] },
+          ]
+        for(let i=0;i<=players.length;i++){
+            console.log(players[i])
+            service.addPlayer(players[i])
+        }
         service.init()
         service.shuffle()
-        service.checkIfLowest(24)
+        service.setPlayerCard()
+        service.setLowestValue()
+        service.checkIfGameOver()
     }
 }
 
